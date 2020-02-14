9398febc29a8fd0d7155c470e59896bb96c90a6c
LEONARDO MONTAGNOLLI MARTINS <leonardo.mmartins2@senacsp.edu.br>
2020-02-14 14:48:06 -0300

    Calculadora java pelo vscode

---
+++ Calculadora.java
@@ -0,0 +1,34 @@
+
+public class Calculadora () {
+private float numero1;
+private float numero2;
+private float resultado;
+
+
+    public float getNumero1 () {
+        return numero1;
+    }
+
+    public void setNumero1 (float numero) {
+        this.numero1 = numero;
+    }
+
+    public float getNumero2 () {
+        return numero2;
+    }
+
+    public void setNumero2 (float numero) {
+        this.numero2 = numero;
+    }
+
+    public float getResultado () {
+        return resultado;
+    }
+
+    public void resultado (float n1, float n2) {
+        this.numero1 = n1;
+        this.numero2 = n2;
+        this.resultado = n1+n2;
+    }
+
+}
